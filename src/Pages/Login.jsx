5497f6274b3b1cf1264503cd0f6257961a4e6425
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-07-09 20:55:37 -0300

    feat: criação de função que faz a gestão e direcionamento do usurario durante o login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const navigate = useNavigate();
+  const test = [
+    { userName: 'nome1', userEmail: 'email1', userPassword: 'senha1' },
+    { userName: 'nome2', userEmail: 'email2', userPassword: 'senha2' },
+    { userName: 'nome3', userEmail: 'email3', userPassword: 'senha3' },
+    { userName: 'nome4', userEmail: 'email4', userPassword: 'senha4' },
+  ]; // Testes durante a produção
   const login = () => {
+    const emailTest = 'email6'; // Testes durante a produção
+    const senhaTest = 'senha1'; // Testes durante a produção
+    const userData = test.find(({ userEmail }) => userEmail === emailTest);
+    if (userData) {
+      if (userData.userPassword === senhaTest) {
+        console.log('Senha tá certa'); // Testes durante a produção
+      } else {
+        global.alert('A senha está incorreta');
+      }
+    }
+    if (!userData && global.confirm('Usuario não encontrado. Deseja cria uma conta?')) {
+      navigate('/registration');
+    }
   };
   return (
     <>

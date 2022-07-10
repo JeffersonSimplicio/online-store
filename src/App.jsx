67e8a60f0c577c354a1dd4c0671b7c48a180236c
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-07-10 10:49:35 -0300

    feat: criado uma componente Main, apenas para auxiliar as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Main from './Pages/Main';
 import Login from './Pages/Login';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Login />} />
-      <Route path="/registration" element={<Login />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Login />} />
+      </Routes>
+    </>
   );
 }
 

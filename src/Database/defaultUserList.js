77d8e0789c22c124a40c81b10ea4e6e27c1b775c
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-07-09 21:03:54 -0300

    Criação de um pasta para simular um banco de dados, onde já foi criado um arquivo com uma lista que simula usuarios pre-existentes

---
+++ src/Database/defaultUserList.js
@@ -0,0 +1,17 @@
+const DEFAULT_USER_LIST = [
+  { userName: 'Adimn', userEmail: 'admin@email.com', userPassword: 'adminadmin' },
+  { userName: 'Marcos Silva', userEmail: 'marcos_silva@email.com', userPassword: 'senhaforte' },
+  { userName: 'Miku Nakano', userEmail: 'NakanoMiku@email.com', userPassword: 'MikuItou' },
+  { userName: 'Gustavo Guanabara', userEmail: 'CursoEmVideo@email.com', userPassword: 'AlfredoJunior' },
+  { userName: 'Paula Silveira', userEmail: 'PaulaSilveira@email.com', userPassword: 'paula123' },
+  { userName: 'Adriano Ponte', userEmail: 'Ponte.Adriano@email.com', userPassword: '123ponte456' },
+  { userName: 'Karyna Fraça', userEmail: 'KarynFR@email.com', userPassword: '123password' },
+  { userName: 'Alexandro Lopez', userEmail: 'LopezAle@email.com', userPassword: 'criptografia' },
+  { userName: 'Marin Kitagawa', userEmail: 'MarinKitagawa@email.com', userPassword: 'NathaliaGuillen' },
+  { userName: 'Jefferson Simplico', userEmail: 'Jefferson_Simplico@email.com', userPassword: 'CriadorDissoTudo' },
+];
+export default DEFAULT_USER_LIST;
+// All data in this file are fictitious, and its sole purpose is to simulate data from a server.
+
+// Todos os dados presentes neste arquivo são fictícios,
+// e tem como unicolo objetivo simular dados de um vindos de um servidor.
